8d2ce5bcea9e7798ab966ad4cbd1bdb8fb466be8
kenneth.llanto <kenneth.llanto@xero.com>
2018-07-20 17:15:20 +1000

    Second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+     username: '',
+     password: ''
+    }
+   }
+
   render() {
     return (
       <div className="App">
@@ -12,14 +21,27 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {this.createButton()}
+        </p>      
+        {React.createElement('button', { className: 'greeting' }, 'Hello, world!')}
+        <form className='Login'>
+          <label>Username</label>
+          <input id='email' onChange={this.handleInputChange} name='email' type='text' />
+          <label>Password</label>
+          <input id='password' onChange={this.handleInputChange} name='password' type='password' />
+          <button>Submit</button>
+        </form>
       </div>
+      
     );
   }
-  createButton() {
+  myFunction() {
     return (<button onclick="myFunction()">Click me</button>);
   }
+  handleInputChange = (event) => {
+    this.setState({
+     [event.target.name]: event.target.value
+    })
+   }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount, render } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('button Component', () => {
+ 
+  // make our assertion and what we expect to happen 
+  it('should render without throwing an error', () => {
+    expect(shallow(<App />).find('form.login').exists()).toBe(true)
+  })
+  it('renders a email input', () => {
+    expect(shallow(<App />).find('#email').length).toEqual(1)
+   })
+  it('renders a password input', () => {
+    expect(shallow(<App />).find('#password').length).toEqual(1)
+   })
+ });
